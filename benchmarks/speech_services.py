d5c945b251fa46e5d9c4b35e604a8f20663509a5
maejam <git.rejoice553@passmail.net>
2026-09-21 15:28:28 +0200

    fix(benchmarks): update speech services benchmarks
    Use `NarrationScene.generate_narrations` instead of
    `speech_service.generate_speech` in order to serve as a test of the user
    facing method.

--- benchmarks/speech_services.py
+++ benchmarks/speech_services.py
@@ -1,46 +1,78 @@
-import timeit
+import pickle
+import shutil
+import time
 import typing as t
 from pathlib import Path
 
 import tabulate
 
-from manim_narration.audio_utils import convert_to_wav
+from manim_narration import NarrationScene, config
 from manim_narration.speech import (
     ChatterboxService,
     CoquiService,
     GTTSService,
     KokoroService,
 )
-from manim_narration.speech.speech_base import SpeechService
+from manim_narration.tracker import NarrationTracker
 
-SPEECH_SERVICES = [
-    CoquiService(
+path = Path(__file__).parent / "narrations/temp/"
+path.mkdir(exist_ok=True, parents=True)
+config.cache.dir = path
+
+SPEECH_SERVICES = {
+    "coqui": CoquiService(
         "tts_models/multilingual/multi-dataset/xtts_v2",
         language="en",
         speaker="Claribel Dervla",
     ),
-    GTTSService(),
-    KokoroService(),
-    ChatterboxService(language_id="en"),
+    "gtts": GTTSService(),
+    "kokoro": KokoroService(),
+    "chatterbox": ChatterboxService(language_id="en"),
+}
+EXECUTION_MODES: list[t.Literal["sequential", "multithreading", "multiprocessing"]] = [
+    "sequential",
+    "multithreading",
+    "multiprocessing",
 ]
-NUM_EXECUTIONS = 10
-TEXT = (
-    "Narration is the use of a written or spoken commentary "
-    "to convey a story to an audience."
-)
+TEXTS = [
+    "Narration number one",
+    "Narration number two",
+    "Narration number three",
+    "Narration number four",
+    "Narration number five",
+]
+
+scene = NarrationScene()
+scene.set_speech_services(**SPEECH_SERVICES)
 
 
-speech_service: SpeechService
-
-
-def benchmark_speech_service() -> None:
-    path = Path().cwd() / f"benchmarks/narrations/{type(speech_service).__name__}"
-    path.parent.mkdir(exist_ok=True)
-    file = speech_service.generate_speech(
-        TEXT, path.with_suffix(speech_service.file_extension)
+def benchmark_speech_service(
+    speech_service_id: str,
+    mode: t.Literal["sequential", "multithreading", "multiprocessing"],
+) -> float:
+    speech_service = SPEECH_SERVICES[speech_service_id]
+    print(
+        f"\n=============== Benchmarking: {type(speech_service).__name__[:-7]} / {mode}"
+        " ==============="
     )
-    # convert to wav because it is part of the speech generation process
-    file = convert_to_wav(file)
+    t1 = time.perf_counter()
+    tasks = {f"task{i}": text for i, text in enumerate(TEXTS)}
+
+    results = scene.generate_narrations(
+        speech_service_id,
+        mode=mode,
+        ignore_cache=True,
+        **tasks,  # pyright: ignore[reportArgumentType]
+    )
+
+    t2 = time.perf_counter()
+    duration = t2 - t1
+    for k, v in results.items():
+        assert k.startswith("task")
+        assert isinstance(v, NarrationTracker)
+        assert v.audio_file_path.suffix == ".wav"
+    print(f"{len(tasks)} narrations generated in {duration} seconds.")
+    return duration
 
 
 def build_markdown() -> t.Generator[list[list[str]], list[str], None]:
@@ -55,22 +87,33 @@ if __name__ == "__main__":
     next(markdown_generator)
     md = []
 
-    for speech_service in SPEECH_SERVICES:
-        # time speech generation
-        res = timeit.timeit(
-            benchmark_speech_service,
-            globals={"speech_service": speech_service},
-            number=NUM_EXECUTIONS,
-        )
+    for speech_service_id in SPEECH_SERVICES:
+        times = []
+        for mode in EXECUTION_MODES:
+            try:
+                res: float | str = benchmark_speech_service(speech_service_id, mode)
+            except (pickle.PicklingError, OSError) as e:
+                res = "NA"
+                print(f"{mode} execution is not supported for this speech service: ", e)
+
+            times.append(res)
 
         # send result to build_markdown
+        speech_service = SPEECH_SERVICES[speech_service_id]
         service_name = type(speech_service).__name__[:-7]
+        mean_times = []
+        for time_ in times:
+            try:
+                mean_times.append(str(round(time_ / len(TEXTS), 2)))  # type: ignore[operator]
+            except TypeError:  # "NA"
+                mean_times.append("NA")
+
         md = markdown_generator.send(
             [
                 service_name,
                 "",
                 "",
-                f"{round(res / NUM_EXECUTIONS, 2)} seconds",
+                " / ".join(mean_times),
                 f"[sample](benchmarks/narrations/{service_name}.wav?raw=True)",
             ]
         )
@@ -79,7 +122,7 @@ if __name__ == "__main__":
         "Service",
         "type".ljust(5),
         "Languages".ljust(20),
-        "Inference time (cpu)",
+        "Inference time (cpu)\\*",
         "Audio sample",
     ]
     md_table = tabulate.tabulate(
@@ -87,4 +130,10 @@ if __name__ == "__main__":
         headers=headers,
         tablefmt="github",
     )
-    print(md_table)
+    # delete temp dir
+    shutil.rmtree(path, ignore_errors=True)
+    print("\n\n", md_table)
+    print(
+        f"<sub>\\*Mean time in seconds to generate {len(TEXTS)} short speeches "
+        f"({'/'.join(EXECUTION_MODES)})</sub>"
+    )
